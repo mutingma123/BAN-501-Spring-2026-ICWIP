872cd9e25f7d39322666da906549403715e9395b
Nickolas K Freeman <nick.freeman524@gmail.com>
2026-01-15 10:47:29 -0600

    add material after 20260115 class

--- 1-linear-regression.py
+++ 1-linear-regression.py
@@ -13,7 +13,7 @@ def _():
     import polars as pl
     import seaborn as sns
     import statsmodels.formula.api as smf
-    return np, plt, sns
+    return np, pl, plt, smf, sns
 
 
 @app.cell
@@ -52,7 +52,7 @@ def _(np, plt, sns):
 
 @app.cell
 def _(np, plt, sns):
-    neighbor_radius = 1
+    neighbor_radius = 0.01
     num_neighbors = 100
 
     theta_values = np.linspace(
@@ -84,8 +84,9 @@ def _(neighbor_b_values, neighbor_m_values, np, x, y):
     sse = ((y - y_hat)**2).sum()
     best_sse = sse
 
-    neighbor_min_sse = -np.inf
-    while neighbor_min_sse < best_sse:
+    improvements_made = True
+    while improvements_made:
+        improvements_made = False
         neighbor_m_array = m_hat + neighbor_m_values
         neighbor_b_array = b_hat + neighbor_b_values
 
@@ -99,6 +100,7 @@ def _(neighbor_b_values, neighbor_m_values, np, x, y):
         neighbor_sse = neighbor_squared_errors.sum(axis=1)
         neighbor_min_sse = neighbor_sse.min()
         if  neighbor_min_sse < best_sse:
+            improvements_made = True
             min_index = np.argmin(neighbor_sse)
             m_hat = neighbor_m_array[min_index]
             b_hat = neighbor_b_array[min_index]
@@ -107,43 +109,29 @@ def _(neighbor_b_values, neighbor_m_values, np, x, y):
     print(f' - {m_hat = :.3f}')
     print(f' - {b_hat = :.3f}')
     print(f' - {best_sse = :.3f}')
+    return (best_sse,)
+
+
+@app.cell
+def _(best_sse, y):
+    mean_sse = ((y.mean() - y)**2).sum()
+    1 - best_sse/mean_sse
     return
 
 
 @app.cell
-def _():
-    # m_hat = 0
-    # b_hat = y.min()
+def _(pl, smf, x, y):
+    reg_data = pl.DataFrame({
+        'x': x, 
+        'y': y,
+    })
 
-    # y_hat = m_hat*x + b_hat
-    # sse = ((y - y_hat)**2).sum()
-    # best_sse = sse
-
-    # improvements_made = True
-    # while improvements_made:
-    #     improvements_made = False
-    #     neighbor_m_array = m_hat + neighbor_m_values
-    #     neighbor_b_array = b_hat + neighbor_b_values
-
-    #     neighbor_predictions = (
-    #         neighbor_m_array.reshape(-1, 1) * x 
-    #         + neighbor_b_array.reshape(-1, 1)
-    #     )
-
-    #     neighbor_errors = y.reshape(1, -1) - neighbor_predictions
-    #     neighbor_squared_errors = neighbor_errors**2
-    #     neighbor_sse = neighbor_squared_errors.sum(axis=1)
-    #     neighbor_min_sse = neighbor_sse.min()
-    #     if  neighbor_min_sse < best_sse:
-    #         improvements_made = True
-    #         min_index = np.argmin(neighbor_sse)
-    #         m_hat = neighbor_m_array[min_index]
-    #         b_hat = neighbor_b_array[min_index]
-    #         best_sse = neighbor_min_sse
-
-    # print(f' - {m_hat = :.3f}')
-    # print(f' - {b_hat = :.3f}')
-    # print(f' - {best_sse = :.3f}')
+    ols_reg = smf.ols(
+        data=reg_data,
+        formula = 'y ~ x'
+    )
+    ols_reg = ols_reg.fit()
+    print(ols_reg.summary())
     return
 
 
